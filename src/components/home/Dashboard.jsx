ae7553e3d1ac7092924042559b016eee5624f89c
nerina <ncoronel@primerosystems.com>
2024-09-18 19:51:01 -0300

    Created Dashboard

---
+++ src/components/home/Dashboard.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { allBooks } from "../../data/data";
+import NewBook from "../newBook/NewBook";
+import Books from "../books/Books";
+
+const Dashboard = () => {
+    const [booksFiltered, setBooksFiltered] = useState(allBooks);
+  
+  
+    const saveBookDataHandler = (enteredBookData) => {
+      const bookData = {
+        ...enteredBookData,
+        id: Math.random(),
+      };
+  
+  
+      setBooksFiltered([bookData, ...booksFiltered]);
+    };
+  
+  
+    const searchHandler = (searchInput) => {
+      if (searchInput === "") setBooksFiltered(allBooks);
+  
+  
+      const searchInputUpperCase = searchInput.toUpperCase();
+      const booksSearched = allBooks.filter((book) =>
+        book.bookTitle.toUpperCase().includes(searchInputUpperCase)
+      );
+      setBooksFiltered(booksSearched);
+    };
+  
+  
+    return (
+      <div className="d-flex flex-column align-items-center">
+        <h2>Books Champion App</h2>
+        <p>¡Quiero leer libros!</p>
+        <NewBook onBookDataSaved={saveBookDataHandler} />
+        <Books onSearch={searchHandler} books={booksFiltered} />
+        </div>
+    );
+  };
+  
+  
+  export default Dashboard;
